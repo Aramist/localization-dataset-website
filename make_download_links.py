7444aaa83ce92609d00497021df9160529f83a2f
Aramis Tanelus <atanelus@flatironinstitute.org>
2024-06-06 11:57:58 -0400

    Add test-train splits and pretrained models

--- make_download_links.py
+++ make_download_links.py
@@ -6,6 +6,7 @@ video_dir = Path("/mnt/home/atanelus/ceph/neurips_datasets/videos")
 
 download_dir = Path("/mnt/home/atanelus/public_www/res/download_links")
 links = [
+    # Audio links
     (
         dataset_dir / "audio" / "dyadgerbil-4m-e1_audio.h5",
         download_dir / "dyadgerbil-4m-e1_audio.h5",
@@ -42,6 +43,7 @@ links = [
         Path("/mnt/ceph/users/njoseph/dataset/dataset_3.h5"),
         download_dir / "hexapod-8m-e2_audio.h5",
     ),
+    # Video links
     (
         dataset_dir / "videos" / "sologerbil-4m-e1_video.h5",
         download_dir / "sologerbil-4m-e1_video.h5",
@@ -57,6 +59,56 @@ links = [
     (
         dataset_dir / "videos" / "dyadgerbil-4m-e1_video.h5",
         download_dir / "dyadgerbil-4m-e1_video.h5",
+    ),
+    # test train split links
+    (
+        dataset_dir / "test_train_splits" / "mouseearbud-24m-e3_split.zip",
+        download_dir / "mouseearbud-24m-e3_split.zip",
+    ),
+    (
+        dataset_dir / "test_train_splits" / "sologerbil-4m-e1_split.zip",
+        download_dir / "sologerbil-4m-e1_split.zip",
+    ),
+    (
+        dataset_dir / "test_train_splits" / "edison-4m-e1_split.zip",
+        download_dir / "edison-4m-e1_split.zip",
+    ),
+    (
+        dataset_dir / "test_train_splits" / "gerbilearbud-4m-e1_split.zip",
+        download_dir / "gerbilearbud-4m-e1_split.zip",
+    ),
+    (
+        dataset_dir / "test_train_splits" / "speaker-4m-e1_split.zip",
+        download_dir / "speaker-4m-e1_split.zip",
+    ),
+    (
+        dataset_dir / "test_train_splits" / "hexapod-8m-e2_split.zip",
+        download_dir / "hexapod-8m-e2_split.zip",
+    ),
+    # Pretrained models
+    (
+        dataset_dir / "pretrained_models" / "edison-4m-e1_pretrained.zip",
+        download_dir / "edison-4m-e1_pretrained.zip",
+    ),
+    (
+        dataset_dir / "pretrained_models" / "sologerbil-4m-e1_pretrained.zip",
+        download_dir / "sologerbil-4m-e1_pretrained.zip",
+    ),
+    (
+        dataset_dir / "pretrained_models" / "speaker-4m-e1_pretrained.zip",
+        download_dir / "speaker-4m-e1_pretrained.zip",
+    ),
+    (
+        dataset_dir / "pretrained_models" / "gerbilearbud-4m-e1_pretrained.zip",
+        download_dir / "gerbilearbud-4m-e1_pretrained.zip",
+    ),
+    (
+        dataset_dir / "pretrained_models" / "mouseearbud-24m-e3_pretrained.zip",
+        download_dir / "mouseearbud-24m-e3_pretrained.zip",
+    ),
+    (
+        dataset_dir / "pretrained_models" / "hexapod-8m-e2_pretrained.zip",
+        download_dir / "hexapod-8m-e2_pretrained.zip",
     ),
 ]
 
